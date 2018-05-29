4d7ecbc16702a619b76443cab5327a8b88def40e
KingHenryJr <henryyihengzhang@gmail.com>
2018-05-28 23:11:08 -0400

    updated backend

--- app/controllers/api/v1/poems_controller.rb
+++ app/controllers/api/v1/poems_controller.rb
@@ -5,18 +5,5 @@ class Api::V1::PoemsController < ApplicationController
     poems = Poem.all
     render json: poems
   end
-
-  private
-
-  def get_poems
-    url = 'http://poetrydb.org/author/William%20Shakespeare'
-    response = HTTParty.get(url)
-
-    parsed = response.parsed_response
-
-    parsed.each do |poems|
-      Poem.create(title: poems["title"], lines: poems["lines"])
-    end
-
-  end
+  
 end
